ecd9f041c86394a73f12290f306b3448744fdced
eradevapps2015 <eradevapps2015@users.noreply.github.com>
2015-05-10 18:18:41 +0600

    Update app.js

--- www/js/app.js
+++ www/js/app.js
@@ -7,9 +7,9 @@ angular.module('starter', ['ionic' ,'starter.controllers','starter.directives','
 
 .run(function($ionicPlatform,  $ionicPopup, $cordovaDevice) {
   $ionicPlatform.ready(function() {
-    //alert("App Controller");
+    alert("App Controller");
   var uu = $cordovaDevice.getUUID();
-	 // alert("UU ID :"+uu);
+	 alert("UU ID App:"+uu);
     // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
     // for form inputs)
     if (window.cordova && window.cordova.plugins.Keyboard) {
@@ -23,6 +23,17 @@ angular.module('starter', ['ionic' ,'starter.controllers','starter.directives','
 			$ionicPlatform.registerBackButtonAction(function (event) {
 			event.preventDefault();
 			}, 100) 
+			
+		
+			if(window.Connection) {
+                if(navigator.connection.type == Connection.NONE) {
+                    $ionicPopup.confirm({
+                        title: "Internet Disconnected",
+                        content: "The internet is disconnected on your device. Please Connect Internet"
+                    })
+                    
+                }
+            }
  
 		
   });
