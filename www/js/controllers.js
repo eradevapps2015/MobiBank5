4977fc4c6b67b55f9864be9a2ed5b3903ed13e6c
eradevapps2015 <eradevapps2015@users.noreply.github.com>
2015-05-11 12:55:30 +0600

    Update controllers.js

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -125,13 +125,72 @@ document.addEventListener("deviceready", function() {
 		$scope.user = { uname:'era@mybank.com'};
 		
 
-			 $scope.login1 = function(user) {
-			$state.go('app.welcome');
+			 $scope.login = function(user) {
+								 var query = "SELECT user_id FROM useridinfo where user_id=?";
+										  $cordovaSQLite.execute(db, query,[user.uname]).then(function(res) {
+											if(res.rows.length > 0) {			
+												for(var i=0; i<res.rows.length; i++){
+													
+												//$scope.branch_code_values=	res.rows.item(i).branch_code;				
+												// $scope.results.push(res.rows.items(i));
+												$state.go('app.welcome');	
+
+												} 
+												
+											} else {
+												
+												
+											 
+														//Begin Else Query
+														var queryUserID = "SELECT user_id FROM useridinfo";
+												 
+													$cordovaSQLite.execute(db, queryUserID).then(function(res) {
+														if(res.rows.length > 0) {			
+															for(var i=0; i<res.rows.length; i++){
+															var uid=res.rows.item(i).user_id;
+															
+															//Begin Update
+															 var queryUserIDUpdate = "UPDATE useridinfo set user_id=? where user_id=?";
+																 $cordovaSQLite.execute(db, queryUserIDUpdate,[user.uname,uid]).then(function(res) {
+																  alert("Updated Successfully");
+																	$state.go('app.welcome');																  
+																}, function (err) {
+																   // console.error(err);
+																	 alert("Error Method");
+																});
+															//End Update
+
+															} 
+															
+														} else {
+														 
+															//Begin For Insert
+																var insertqQuery = "INSERT INTO useridinfo (user_id) VALUES (?)";
+																	 $cordovaSQLite.execute(db, insertqQuery,[user.uname]).then(function(res) {
+																alert("Insert successfully !");
+																$state.go('app.welcome');
+																}, function (err) {
+																   // console.error(err);
+																	 alert("Error Method");
+																	 
+																});
+															//End For Inser
+														}
+													}, function (err) {
+													   // console.error(err);
+														 alert("Error Method");
+													});
+													//End Else Query
+											}
+										}, function (err) {
+										   // console.error(err);
+											 alert("Error Method");
+										});
 		  };
 		 
 
     
-	$scope.login= function (user) {
+	$scope.login1= function (user) {
 	
        				
 		if(!user || ! user.uname){
