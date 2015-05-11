2ff0402062568fa715e55128df99f6c2b563f821
eradevapps2015 <eradevapps2015@users.noreply.github.com>
2015-05-11 10:29:45 +0600

    Update controllers.js

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -7,9 +7,9 @@ angular.module('starter.controllers', [])
 	
 	.controller('SignInCtrl', function($ionicPlatform,$scope, $state, $http, $rootScope, $ionicLoading, $timeout,$ionicPopup,$filter,$cordovaSQLite,$cordovaDevice) {
 	//$urlRouterProvider.otherwise("/welcome/home");
-	ionic.Platform.ready(function(){
-  alert("controller");
-});
+document.addEventListener("deviceready", function() {
+    alert("controlle ggggggggggggggggggggggggggggr");
+  }, false);
 
         
 
