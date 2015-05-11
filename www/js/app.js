908bf001963cee675635429d3f821dc86b70ee09
eradevapps2015 <eradevapps2015@users.noreply.github.com>
2015-05-11 12:18:33 +0600

    Update app.js

--- www/js/app.js
+++ www/js/app.js
@@ -6,44 +6,7 @@
 angular.module('starter', ['ionic' ,'starter.controllers','starter.directives','starter.services','ngCordova'])
 
 .run(function($ionicPlatform,  $ionicPopup, $cordovaDevice) {
-  $ionicPlatform.ready(function() {
-     alert("controlle AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-   
-  var uu = $cordovaDevice.getUUID();
-/*  $ionicPopup.alert({  
-    	title:'App Controller'+uu,
-      //template:'From date'
-	  })*/
-	// alert("UU ID App:"+uu);
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    
-		if(window.Connection) {
-                if(navigator.connection.type == Connection.NONE) {
-                    $ionicPopup.confirm({
-                        title: "Internet Disconnected",
-                        content: "The internet is disconnected on your device. Please Connect Internet"
-                    })
-                    
-                }
-            }
-    
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-	
-			$ionicPlatform.registerBackButtonAction(function (event) {
-			event.preventDefault();
-			}, 100) 
-			
-		
  
-		
-  });
 })
 
 
